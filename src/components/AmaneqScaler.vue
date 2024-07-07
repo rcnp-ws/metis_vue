a56f339a2e1b2339f634204460c1096fa4bb2bf1
Shinsuke OTA <ota@rcnp.s.u-tokyo.ac.jp>
2024-07-08 08:52:55 +0900

    [scaler,tab] add container and scaler view

--- src/components/AmaneqScaler.vue
+++ src/components/AmaneqScaler.vue
@@ -2,10 +2,18 @@
    <div>
       Scaler
       <LocalTimestamp></LocalTimestamp>
-      {{msg.timestamp}}
+      Remote : {{msg.timestamp}} at {{ msg.ip }}
+
+<!--
       <table>
          <tr v-for="scr in msg.scr" :key="scr.ch">
            <td>{{ scr.ch }}</td><td>{{ scr.val }}</td>
+         </tr>
+      </table>
+-->      
+      <table>
+         <tr v-for="(scrcol,irow) in scrs" :key="irow">
+            <td v-for="(aScr,icol) in scrcol" :key="icol">{{ aScr }}</td>
          </tr>
       </table>
    </div>
@@ -18,23 +26,51 @@ import LocalTimestamp from './LocalTimestamp.vue';
 
 export default {
    components: { LocalTimestamp },
+   props: {
+      ip: String,
+   },
    data() {
       return {
-         msg: {"device":"IDLE"}
+         msg: {"device":"IDLE"},
+         nTot: 32,
+         nCols: 1,
+         nRows: 32,
+         scrs: [],
+         // 表を整形する
       }
    },
    methods: {
       update() {
-         axios.get('http://localhost:8000/amqscr/read/192.168.2.160')
+         axios.get('http://localhost:8000/scaler/read/'+this.ip)
         .then((response) => {
             //console.log(response.data);
-                this.msg = response.data;
+               this.msg = response.data;
+               this.nTot = this.msg.scr.length;
+               //console.log(this.msg);
+               if (this.nTot > 0) {
+                  this.nCols = Math.ceil(this.nTot / this.nRows);
+                  for (let iscr = 0; iscr < this.nRows; ++iscr ) {
+                     this.scrs[iscr] = [];
+                  }
+                  for (let iscr = 0; iscr < this.nTot; ++iscr ) {
+                     let icol = Math.floor(iscr/this.nRows);
+                     let irow = iscr%this.nRows;
+                     this.scrs[irow][2 * icol + 0] = this.msg.scr[irow + icol * this.nRows].ch;
+                     this.scrs[irow][2 * icol + 1] = this.msg.scr[irow + icol * this.nRows].val;
+                  }
+               } else {
+                  this.nTot = 0;
+                  this.nRows = 0;
+                  this.nCols = 0;
+               }
             });
             setTimeout(() => { this.update(); }, 1000);
       }
    },
    mounted() {
-      this.update()
+      //console.log(this.ip);
+      this.update();
+
    }
 }
 </script>
---
+++ src/examples/TabExample.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="bg-gradient-to-r from-blue-300 to-blue-500 min-h-screen">
+    <h1 class="text-xl font-bold text-white mb-4 w-11/12 lg:w-10/12 mx-auto">
+      Variant: Vertical(default)
+    </h1>
+
+    <tab-container class="w-11/12 lg:w-10/12 mx-auto mb-16" :tabList="tabList">
+      <template v-slot:tabPanel-1> Content 1 </template>
+      <template v-slot:tabPanel-2> Content 2 </template>
+      <template v-slot:tabPanel-3> {{tabList[2]}} </template>
+      <template v-slot:tabPanel-4> {{tabList[3]}}</template>
+    </tab-container>
+
+    <h1 class="text-xl font-bold text-white mb-4 w-11/12 lg:w-10/12 mx-auto">
+      Variant: Horizontal
+    </h1>
+
+    <tab-container
+      class="w-11/12 lg:w-10/12 mx-auto"
+      :tabList="tabList"
+      variant="horizontal"
+    >
+      <template v-slot:tabPanel-1> Content 1 </template>
+      <template v-slot:tabPanel-2> Content 2 </template>
+      <template v-slot:tabPanel-3> <amaneq-scaler :ip=tabList[2]></amaneq-scaler> </template>
+      <template v-slot:tabPanel-4> <amaneq-scaler :ip=tabList[3]></amaneq-scaler> </template>
+    </tab-container>
+
+    <div class="text-center py-12">
+      <a
+        href="https://lucaspezzano.medium.com/subscribe"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-white font-semibold underline"
+      >
+        Get straight to your email next time I post a demo or article
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import TabContainer from '../components/TabContainer.vue';
+import AmaneqScaler from '../components/AmaneqScaler.vue'
+
+
+export default {
+  components: {
+    TabContainer,
+    AmaneqScaler,
+  },
+  data() {
+    return {
+      tabList: ["Tab 1", "Tab 2", "192.168.10.1", "192.168.10.2"],
+    };
+  },
+};
+</script>
+
