1a05fe10f7d2248fdd5c225d1b7bc7355870d5cb
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2024-07-15 19:00:29 +0900

    auto run get ctl was added

--- src/components/NestDAQController.vue
+++ src/components/NestDAQController.vue
@@ -30,17 +30,17 @@
    <div> Stop time: {{daq_stop_time}} </div>
    <div>
       <span>Auto run change: </span>
-      <span v-if="auto_run_change_mode_enabled == true">Enabled, Elapsed time: {{run_elapsed_time}} sec / Duration: {{key_val_read['run_info:auto_run_change_dur']}} sec</span>
+      <span v-if="key_val_read['run_info:auto_run_change_mode'] == 'enabled'">Enabled, Elapsed time: {{run_elapsed_time}} sec / Duration: {{key_val_read['run_info:auto_run_change_dur']}} sec</span>
       <span v-else> Disabled</span>
    </div>
    <div>
       <span v-if="daq_state=='IDLE'&&daq_controllable==true">
-         <button class="btn btn-blue" @click='daq_start()'> Start </button> <span></span>
+         <button class="btn btn-blue" @click='state_change(["PRE START","CONNECT","INIT TASK","RUN","POST START"])'> Start </button> <span></span>
       </span><span v-else>
          <button class="btn btn-blue-disabled"> Start </button> <span></span>
       </span>
       <span v-if="daq_state=='RUNNING'&&daq_controllable==true">
-         <button class="btn btn-blue" @click='daq_stop()'> Stop </button> <span></span>
+         <button class="btn btn-blue" @click='state_change(["PRE STOP", "STOP","RESET TASK","RESET DEVICE", "POST STOP"])'> Stop </button> <span></span>
       </span><span v-else>
          <button class="btn btn-blue-disabled"> Stop </button> <span></span>
       </span>
@@ -124,20 +124,40 @@
          <button class="btn btn-gray" @click='state_change(["END"])'> End devices </button>
           ⊳ Exitting
       </div>
-      <div> State change start UNIX time: {{state_change_start_time_in_sec}}, Duration: {{state_change_duration_in_sec}} sec / Timeout: {{state_change_timeout_in_sec}} sec</div>
+      <div> State change start UNIX time: {{state_change_start_time_in_sec}}, Elapsed time: {{state_change_elapsed_time_in_sec}} sec / Timeout: {{state_change_timeout_in_sec}} sec</div>
       <div>&nbsp;</div>
       <div>
-         <span class="text-l font-bold"> Auto run change</span> <span></span>
-         <span><input type="checkbox" v-model="auto_run_change_mode_enabled">: </span>
-         <span v-if="auto_run_change_mode_enabled == true">Enabled</span>
+         <span class="text-l font-bold"> Auto run change:
+         <span v-if="key_val_read['run_info:auto_run_change_mode'] == 'enabled'">Enabled</span>
          <span v-else> Disabled</span>
+         </span>
+      </div>
+      <div v-if="tabID == key_val_read['run_info:auto_run_change_ctl_holder']">
+         <span v-if="key_val_read['run_info:auto_run_change_mode'] == 'enabled'">
+            <button  class="btn btn-gray-disabled"> Enable </button> <span></span>
+            <button  class="btn btn-gray" @click="auto_run_change_disable()"> Disable </button> <span></span>             
+         </span>
+         <span v-else>
+            <button  class="btn btn-gray" @click="auto_run_change_enable()"> Enable </button> <span></span>
+            <button  class="btn btn-gray-disabled"> Disable </button> <span></span>
+         </span>
+         <button  class="btn btn-gray-disabled"> Get Control </button> <span></span>
+      </div>
+      <div v-else>
+         <button  class="btn btn-gray-disabled"> Enable </button> <span></span>
+         <button  class="btn btn-gray-disabled"> Disable </button> <span></span>
+         <button  class="btn btn-gray" @click="auto_run_change_get_ctl()"> Get Control </button> <span></span>      
       </div>
       <div> Duration in sec (read): {{key_val_read['run_info:auto_run_change_dur']}} </div>
       <div>
          Duration in sec (set): <input size="5" class="inpt" type="text" v-model="key_val_set['run_info:auto_run_change_dur']"> <span></span>
          <button  class="btn btn-gray" @click="key_set('run_info:auto_run_change_dur')"> Set </button> <span></span>
       </div>
-      <div> Run start UNIX time {{run_start_unix_time}}, Elapsed time: {{run_elapsed_time}} sec</div>
+      <div v-if="tabID == key_val_read['run_info:auto_run_change_ctl_holder']"> Now you HAVE control! </div>
+      <div v-else> Now you do NOT have control...</div>
+      <div> Tab ID of this tab: {{tabID}} </div>
+      <div> Tab ID of control holder: {{key_val_read['run_info:auto_run_change_ctl_holder']}} </div>
+      <div> Run start UNIX time {{key_val_read['run_info:run_start_unix_time']}}, Elapsed time: {{run_elapsed_time}} sec / Duration : {{key_val_read['run_info:auto_run_change_dur']}} sec</div>
       <div>&nbsp;</div>
       <div class="text-l font-bold"> Hook scripts</div>
       <table>
@@ -215,7 +235,10 @@
     @apply bg-gray-500 text-white opacity-50 cursor-not-allowed;
   }
   .inpt {
-    @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
+    @apply bg-gray-50 border border-gray-300 text-gray-900
+    text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500
+    p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
   }
 </style>
 
@@ -226,8 +249,9 @@ export default {
    data() {
       return {
          fastapi_uri: "http://ata03:8000",
+         tabID: 0,
          state_change_start_time_in_sec: 0,
-         state_change_duration_in_sec: 0,
+         state_change_elapsed_time_in_sec: 0,
          state_change_timeout_in_sec: 20,
          state_change_error: false,
          daq_controllable: true,
@@ -235,8 +259,6 @@ export default {
          daq_start_time: "",
          daq_stop_time: "",
          expert_mode_enabled: false,
-         auto_run_change_mode_enabled: false,
-         run_start_unix_time: 0,
          run_elapsed_time: 0,
          daq_state_destination: {
             "CONNECT"      : "DEVICE READY",
@@ -248,24 +270,30 @@ export default {
             "END"          : "NO PROCESS"
          },
          key_val_read: {
-            "run_info:pre_start_script"    : "",
-            "run_info:post_start_script"   : "",
-            "run_info:pre_stop_script"     : "",
-            "run_info:post_stop_script"    : "",
-            "run_info:run_number"          : "",
-            "run_info:latest_run_number"   : "",
-            "run_info:run_comment"         : "",
-            "run_info:auto_run_change_dur" : ""
+            "run_info:pre_start_script"           : "",
+            "run_info:post_start_script"          : "",
+            "run_info:pre_stop_script"            : "",
+            "run_info:post_stop_script"           : "",
+            "run_info:run_number"                 : "",
+            "run_info:latest_run_number"          : "",
+            "run_info:run_comment"                : "",
+            "run_info:run_start_unix_time"        : "",
+            "run_info:auto_run_change_mode"       : "",
+            "run_info:auto_run_change_dur"        : "",
+            "run_info:auto_run_change_ctl_holder" : ""
          },
          key_val_set: {
-            "run_info:pre_start_script"    : "",
-            "run_info:post_start_script"   : "",
-            "run_info:pre_stop_script"     : "",
-            "run_info:post_stop_script"    : "",
-            "run_info:run_number"          : "",
-            "run_info:latest_run_number"   : "",
-            "run_info:run_comment"         : "",
-            "run_info:auto_run_change_dur" : ""
+            "run_info:pre_start_script"           : "",
+            "run_info:post_start_script"          : "",
+            "run_info:pre_stop_script"            : "",
+            "run_info:post_stop_script"           : "",
+            "run_info:run_number"                 : "",
+            "run_info:latest_run_number"          : "",
+            "run_info:run_comment"                : "",
+            "run_info:run_start_unix_time"        : "",
+            "run_info:auto_run_change_mode"       : "",
+            "run_info:auto_run_change_dur"        : "",
+            "run_info:auto_run_change_ctl_holder" : ""
          },
          hook_return: {
             "run_info:pre_start_script"  : "",
@@ -291,37 +319,62 @@ export default {
          setTimeout(() => { this.update(); }, 1000);
       },
       check_daq_state(){
-        axios.get(this.fastapi_uri+'/nestdaq/status/')
-        .then((response) => {
-           let res = response.data;
-           if (Object.keys(res).length == 0) {
-              this.daq_state = "NO PROCESS";
-              return;
-           }
-           let mismatch_flag = false;
-           for (let key in res){
-             if (res[key] != Object.values(res)[0]) {
-                 mismatch_flag = true;
-              }
-           }
-           if(mismatch_flag == false){
-              this.daq_state = Object.values(res)[0];
-           }else{
-              this.daq_state = "";
-           }
-        }).catch((error)=>{console.log(error.data);});
+         axios.get(this.fastapi_uri+'/nestdaq/status/')
+         .then((response) => {
+            let res = response.data;
+            if (Object.keys(res).length == 0) {
+               this.daq_state = "NO PROCESS";
+               return;
+            }
+            let mismatch_flag = false;
+            for (let key in res){
+              if (res[key] != Object.values(res)[0]) {
+                  mismatch_flag = true;
+               }
+            }
+            if(mismatch_flag == false){
+               this.daq_state = Object.values(res)[0];
+            }else{
+               this.daq_state = "";
+            }
+         }).catch((error)=>{console.log(error.data);});
       },
       check_auto_run_change(){
-         if (this.run_start_unix_time > 0 && this.daq_state == "RUNNING") {
-            this.run_elapsed_time = parseInt(Date.now()/1000 - this.run_start_unix_time);
+         if (this.tabID == this.key_val_read['run_info:auto_run_change_ctl_holder']){
+            this.key_expire('run_info:auto_run_change_mode',10);
+            this.key_expire('run_info:auto_run_change_ctl_holder',10);
+         }
+         if (this.key_val_read['run_info:run_start_unix_time'] > 0 && this.daq_state == "RUNNING") {
+            this.run_elapsed_time = parseInt(Date.now()/1000 - this.key_val_read['run_info:run_start_unix_time']);
             if ((this.run_elapsed_time >= this.key_val_read['run_info:auto_run_change_dur'])&&
-                (this.auto_run_change_mode_enabled == true)&&
+                (this.key_val_read['run_info:auto_run_change_mode'] == "enabled")&&
+                (this.tabID == this.key_val_read['run_info:auto_run_change_ctl_holder'])&&
                 (this.daq_controllable == true)) {
-               this.run_start_unix_time = 0;
+               this.key_val_set['run_info:run_start_unix_time'] = 0;
+               this.key_set('run_info:run_start_unix_time');
                this.state_change(["PRE STOP", "STOP","RESET TASK","RESET DEVICE", "POST STOP",
                "PRE START","CONNECT","INIT TASK","RUN","POST START"]);
             }
          }
+      },
+      auto_run_change_enable(){
+         if (this.key_val_read['run_info:auto_run_change_mode'] != "enabled"&&
+             (this.tabID == this.key_val_read['run_info:auto_run_change_ctl_holder'])) {
+             this.key_val_set['run_info:auto_run_change_mode'] = "enabled";
+             this.key_set('run_info:auto_run_change_mode');
+             this.auto_run_change_get_ctl();
+         }
+      },
+      auto_run_change_disable(){
+         if ((this.key_val_read['run_info:auto_run_change_mode'] == "enabled")&&
+             (this.tabID == this.key_val_read['run_info:auto_run_change_ctl_holder'])) {
+             this.key_val_set['run_info:auto_run_change_mode'] = "";
+             this.key_set('run_info:auto_run_change_mode');
+         }
+      },
+      auto_run_change_get_ctl(){
+         this.key_val_set['run_info:auto_run_change_ctl_holder'] = this.tabID;
+         this.key_set('run_info:auto_run_change_ctl_holder');
       },
       key_set(key){
          if (key=="run_info:run_comment"){
@@ -329,7 +382,15 @@ export default {
                this.key_val_set[key] = this.key_val_set[key].substring(0, 255);
             }
          }
+         //this.key_val_read[key] = this.key_val_set[key];
          axios.get(this.fastapi_uri+'/nestdaq/set_path/'+key+'/'+this.key_val_set[key])
+         .then(()=>{            
+            this.key_read(key);
+         })
+         .catch((error)=>{console.log(error.data);});
+      },
+      key_expire(key, time){
+         axios.get(this.fastapi_uri+'/expire/'+key+'/'+time)
          .catch((error)=>{console.log(error.data);});
       },
       key_clear(key){
@@ -347,12 +408,6 @@ export default {
             this.key_val_set[key] = response.data["message"];
          }).catch((error)=>{console.log(error.data);});
       },
-      daq_start(){
-         this.state_change(["PRE START","CONNECT","INIT TASK","RUN","POST START"]);
-      },
-      daq_stop(){
-         this.state_change(["PRE STOP", "STOP","RESET TASK","RESET DEVICE", "POST STOP"]);
-      },
       exec_hook_script(key){
          return new Promise ((resolve, reject) => {
             axios.get(this.fastapi_uri+'/get/'+key)
@@ -368,12 +423,16 @@ export default {
       },
       wait_for_state_change(daq_state_dest, start_time_in_sec){
          return new Promise ((resolve, reject) => {
-            this.state_change_duration_in_sec = parseInt(Date.now() / 1000 - start_time_in_sec);
-            if (this.state_change_duration_in_sec >= this.state_change_timeout_in_sec) {
+            this.state_change_elapsed_time_in_sec = parseInt(Date.now() / 1000 - start_time_in_sec);
+            if (this.state_change_elapsed_time_in_sec >= this.state_change_timeout_in_sec) {
                reject();
             }else{
                if (daq_state_dest != this.daq_state) {
-                  setTimeout(()=>{ this.wait_for_state_change(daq_state_dest, start_time_in_sec).then(()=>{resolve();}).catch(()=>{reject();}); }, 1000);
+                  setTimeout(()=>{
+                     this.wait_for_state_change(daq_state_dest, start_time_in_sec)
+                     .then(()=>{resolve();})
+                     .catch(()=>{reject();});
+                  }, 1000);
                }else{
                   resolve();
                }
@@ -394,7 +453,8 @@ export default {
          }
          if (cmd_arr[0] == "RUN") {
             this.daq_start_time = new Date();
-            this.run_start_unix_time = parseInt(Date.now()/1000);
+            this.key_val_set['run_info:run_start_unix_time'] = parseInt(Date.now()/1000);
+            this.key_set('run_info:run_start_unix_time');
             this.daq_stop_time = "";
             axios.get(this.fastapi_uri+'/get/run_info:run_number')
             .then((response) => {
@@ -436,8 +496,11 @@ export default {
    mounted() {
       this.update();
       for (let key in this.key_val_read){
-        this.key_init(key);
+         this.key_init(key);
       }
+      this.tabID = sessionStorage.tabID ? 
+         sessionStorage.tabID : 
+         sessionStorage.tabID = parseInt((Math.random())*1e10);
    }
 }
 </script>
